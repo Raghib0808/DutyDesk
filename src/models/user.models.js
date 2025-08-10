900fa1f5e21c261cb09b896ae19791ead191037b
Raghib <raghibanis72@gmail.com>
2025-08-11 00:09:11 +0530

    added express hooks

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { mongo } from "mongoose";
-
+import bcrypt from 'bcryptjs'
+import crypto from 'crypto'
 // user schema
 const UserSchema = new mongoose.Schema({
         avatar:{
@@ -55,5 +56,53 @@ const UserSchema = new mongoose.Schema({
         }
 
 },{timestamps:true})
+
+
+// hashing the password before storing it in the db
+UserSchema.pre("save",async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+
+// signing jwt token
+UserSchema.methods.generateAccessToken=async function(){
+    return jwt.sign(
+        {
+            id:this._id,
+            userName:this.userName,
+            email:this.email
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+            }
+    )
+}
+
+// generate Refresh token
+UserSchema.methods.generateRefreshToken=async function(){
+    return jwt.sign(
+        {
+            id:this._id,
+            userName:this.userName,
+            email:this.email
+        },
+            process.env.REFERSH_TOKEN_SECRET,
+            {
+                expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+            }
+    )
+}
+
+
+
+
+
+// model methods
+UserSchema.methods.isPasswordCorrect = async function(passowrd){
+        return bcrypt.compare(passowrd,this.passowrd);
+}
 
 export const User =  mongoose.model("User",UserSchema);
